6ebdf91acdd26d92861cd797784681e3ab56b233
ごう\go008 <go00882244go@gmail.com>
2024-08-23 19:07:29 +0900

    [futer] MoveBlock [futer] Stage2 色々

---
+++ Assets/Script/MoveXminus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveXminus : MonoBehaviour
+{
+    public int speed;
+    private Vector3 startPos;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Žæ“¾");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = startPos.x - Mathf.PingPong(Time.time * speed, 3f);
+        rb.MovePosition(new Vector3(posX, startPos.y, startPos.z));
+    }
+}
---
+++ Assets/Script/MoveYminus.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveYminus : MonoBehaviour
+{
+    public int speed;
+    private Vector3 startPos;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Žæ“¾");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(transform.position);
+        float posY = startPos.y - Mathf.PingPong(Time.time * speed, 3f);
+        rb.MovePosition(new Vector3(startPos.x, posY, startPos.z));
+    }
+}
